a057cef083155363937bea5044e886779991423a
simpleneeraj <simplneeraj@gmail.com>
2022-07-18 11:11:17 +0530

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,15 @@
-import '../styles/globals.css';
+import "../styles/globals.scss";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
-import type { AppProps } from 'next/app';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import axios from '../networkAPI/axios';
-import {
-  persistor,
-  store,
-} from '../redux/store';
+import axios from "../networkAPI/axios";
+import { persistor, store } from "../redux/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient({
@@ -28,32 +22,24 @@ function MyApp({ Component, pageProps }: AppProps) {
   });
 
   useEffect(() => {
-    axios.interceptors.request.use(
-      (config:any) => {
-        config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
-        return config;
-      }
-    );
+    axios.interceptors.request.use((config: any) => {
+      config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+      return config;
+    });
   }, []);
 
-  console.log({store:store.getState()});
+  console.log({ store: store.getState() });
 
   return (
-
-<Provider store={store}>
+    <Provider store={store}>
       <PersistGate persistor={persistor}>
-      
-    <QueryClientProvider client={queryClient}>
-    
-      <Component {...pageProps} />
-     
-      <ReactQueryDevtools initialIsOpen={false} />
-      
-    </QueryClientProvider>
-    </PersistGate>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </PersistGate>
     </Provider>
-    
- 
   );
 }
 
