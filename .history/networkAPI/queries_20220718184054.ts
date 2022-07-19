85bdf82982cb290d611ec510193af5a7451c592b
unknown <anamika.tomar@epicwebtechno.in>
2022-07-19 10:49:15 +0530

    chnges

---
+++ .history/networkAPI/queries_20220718184054.ts
@@ -0,0 +1,336 @@
+import { useMutation, useQuery } from "react-query";
+
+import {
+  bannersImages,
+  Category,
+  companyProfle,
+  getApprovalProduct,
+  getBannerImages,
+  getBussinessDetails,
+  getbussinessDetaisById,
+  getCategory,
+  getCompnyProfile,
+  getHomeCategory,
+  getMerchantCredentials,
+  getProductById,
+  getProducts,
+  getProductsBycategory,
+  getPublicProduct,
+  getSubCategory,
+  login,
+  register,
+  sellProduct,
+  sendEmail,
+  subCategory,
+  updateBannerImage,
+  updateCategory,
+  updateDeclineProduct,
+  updateProduct,
+  updateSellerProduct,
+  updatesubCategory,
+  UserDetails,
+} from "./api";
+import {
+  bannerImages,
+  bannerImagesUpdate,
+  Login,
+  productCategory,
+  productSubCategory,
+  productUpdateSubCategoryType,
+  Register,
+  updateCategoryType,
+  UserDetail,
+  UserProduct,
+  userProductForDeclined,
+  userProductForUpdate,
+  UserProductType,
+  userProfile,
+} from "./types";
+
+export const useLogin = () =>
+  useMutation(
+    ({ email, password }: Login): Promise<Object> => login(email, password)
+  );
+
+export const useRegister = () =>
+  useMutation(
+    ({ email, password, role }: Register): Promise<Object> =>
+      register(email, password, role)
+  );
+export const useUserDetails = () =>
+  useMutation(
+    ({
+      Merchant_Name,
+      Merchant_Address,
+      TypesOf_Bussiness,
+      SubTypeOf_bussiness,
+      Merchant_Designation,
+      Year_of_establishment,
+      GST_No,
+      PAN_No,
+    }: UserDetail): Promise<Object> =>
+      UserDetails(
+        Merchant_Name,
+        Merchant_Address,
+        TypesOf_Bussiness,
+        SubTypeOf_bussiness,
+        Merchant_Designation,
+        Year_of_establishment,
+        GST_No,
+        PAN_No
+      )
+  );
+
+export const useCompanyProfile = () =>
+  useMutation(
+    ({ company_Name, description }: userProfile): Promise<Object> =>
+      companyProfle(company_Name, description)
+  );
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+export const userProduct = () =>
+  useMutation(
+    ({
+      Vendor_Id,
+      auther_Id,
+      vendors_name,
+      product_name,
+      TypesOf_Bussiness,
+      manufacturer_phone_no,
+      manufacturer_address,
+      brand,
+      product_image1,
+      product_image2,
+      product_image3,
+      product_image4,
+      product_image5,
+
+      category,
+      sub_category,
+
+      price,
+      product_Specification,
+      additionalSpecification,
+      product_description,
+      capacity,
+      model_no,
+      videos,
+    }: UserProduct): Promise<Object> =>
+      sellProduct(
+        Vendor_Id,
+        auther_Id,
+        vendors_name,
+        product_name,
+        TypesOf_Bussiness,
+        manufacturer_phone_no,
+        manufacturer_address,
+        brand,
+        product_image1,
+        product_image2,
+        product_image3,
+        product_image4,
+        product_image5,
+
+        category,
+        sub_category,
+        price,
+        product_Specification,
+        additionalSpecification,
+        product_description,
+        capacity,
+        model_no,
+        videos
+      )
+  );
+// for Update Product By Merchant
+export const useUpdateMerchantProduct = () =>
+  useMutation(
+    ({
+      Vendor_Id,
+      auther_Id,
+      vendors_name,
+      product_name,
+      TypesOf_Bussiness,
+      manufacturer_phone_no,
+      manufacturer_address,
+      brand,
+      product_image1,
+      product_image2,
+      product_image3,
+      product_image4,
+      product_image5,
+
+      category,
+      sub_category,
+
+      price,
+      product_Specification,
+      additionalSpecification,
+      product_description,
+      capacity,
+      model_no,
+      videos,
+      id,
+    }: UserProductType): Promise<Object> =>
+      updateSellerProduct(
+        Vendor_Id,
+        auther_Id,
+        vendors_name,
+        product_name,
+        TypesOf_Bussiness,
+        manufacturer_phone_no,
+        manufacturer_address,
+        brand,
+        product_image1,
+        product_image2,
+        product_image3,
+        product_image4,
+        product_image5,
+
+        category,
+        sub_category,
+        price,
+        product_Specification,
+        additionalSpecification,
+        product_description,
+        capacity,
+        model_no,
+        videos,
+        id
+      )
+  );
+/// for Product Approval
+export const useUpdateProduct = () =>
+  useMutation(
+    ({ isApproved, id }: userProductForUpdate): Promise<Object> =>
+      updateProduct(isApproved, id)
+  );
+
+export const useDeclinedProduct = () =>
+  useMutation(
+    ({ isDeclined, status, id }: userProductForDeclined): Promise<Object> =>
+      updateDeclineProduct(isDeclined, status, id)
+  );
+
+export const useCategory = () =>
+  useMutation(
+    ({ category_name, category_image }: productCategory): Promise<Object> =>
+      Category(category_name, category_image)
+  );
+
+export const useUpdateCategory = () =>
+  useMutation(
+    ({
+      category_name,
+      category_image,
+      id,
+    }: updateCategoryType): Promise<Object> =>
+      updateCategory(category_name, category_image, id)
+  );
+
+// SubCategory ============================
+export const useSubCategory = () =>
+  useMutation(
+    ({
+      category_Id,
+      category_name,
+
+      sub_category_name,
+      sub_category_image,
+    }: productSubCategory): Promise<Object> =>
+      subCategory(
+        category_Id,
+        category_name,
+
+        sub_category_name,
+        sub_category_image
+      )
+  );
+
+export const useUpdateSubCategory = () =>
+  useMutation(
+    ({
+      category_Id,
+      category_name,
+
+      sub_category_name,
+      sub_category_image,
+      id,
+    }: productUpdateSubCategoryType): Promise<Object> =>
+      updatesubCategory(
+        category_Id,
+        category_name,
+
+        sub_category_name,
+        sub_category_image,
+        id
+      )
+  );
+
+export const useBanner = () =>
+  useMutation(
+    ({
+      banner_name,
+      type,
+      banner_image1,
+      banner_image2,
+      banner_image3,
+      banner_image4,
+    }: bannerImages): Promise<Object> =>
+      bannersImages(
+        banner_name,
+        type,
+        banner_image1,
+        banner_image2,
+        banner_image3,
+        banner_image4
+      )
+  );
+
+export const useUpdateBanner = () =>
+  useMutation(
+    ({
+      banner_image1,
+      banner_image2,
+      banner_image3,
+      banner_image4,
+      id,
+    }: bannerImagesUpdate): Promise<Object> =>
+      updateBannerImage(
+        banner_image1,
+        banner_image2,
+        banner_image3,
+        banner_image4,
+        id
+      )
+  );
+export const useProductsByCategory = (category: string) =>
+  useQuery(["products", category], () => getProductsBycategory(category));
+export const useProducts = () => useQuery("products", getProducts);
+export const useGetCompanyProfile = () =>
+  useQuery("companyprofile", getCompnyProfile);
+export const useGetBussinessDetails = () =>
+  useQuery("getBussinessDetails", getBussinessDetails);
+export const useGetMerchantDetails = () =>
+  useQuery("getUser", getMerchantCredentials);
+export const usePublicProduct = () => useQuery("product", getPublicProduct);
+export const useProductForApproval = () =>
+  useQuery("product", getApprovalProduct);
+export const useGetCategory = () => useQuery("category", getCategory);
+export const useGetSubCategory = () => useQuery("subcategory", getSubCategory);
+export const useGetHomeCategory = () => useQuery("category", getHomeCategory);
+export const useGetProductById = (id: string) =>
+  useQuery("myself", (): Promise<any> => getProductById(id));
+
+export const useGetBussinessById = (id: string) =>
+  useQuery("myself1", (): Promise<any> => getbussinessDetaisById(id));
+
+export const useGetBanner = () => useQuery("banner", getBannerImages);
+
+export const useSendEmail = () =>
+  useMutation(({ merchantId, email, phoneNumber, description }: any) =>
+    sendEmail({ merchantId, email, phoneNumber, description })
+  );
+
+// product filter
+// vendor filter
---
+++ .history/pages/view_all_category_20220708175755.tsx
@@ -0,0 +1,61 @@
+import "react-multi-carousel/lib/styles.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import React from "react";
+
+import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+import UserLayout from "../components/User/Layout";
+import { useGetCategory } from "../networkAPI/queries";
+import styles from "../styles/Merchant/view.module.scss";
+
+const AllCategory: NextPage = () => {
+  const data2 = useGetCategory();
+  const category_data = data2.data;
+
+  return (
+    <div>
+
+
+<section className="catlogsection">
+  <div className="container">
+    <div className="row outerbox column-8">
+     
+    <div className={` ${styles.img_box} col-md-3`}>Laundery Machine & Equipments</div>
+        <div className={` ${styles.img_box1} col-md-3`}>Dryclean Machine & Equipment</div>
+        <div className={` ${styles.img_box2} col-md-3`}>Laundry Bags</div>
+        <div className={` ${styles.img_box3} col-md-3`}>Chemical & Detergent</div>
+        <div className={` ${styles.img_box4} col-md-3`}>QR code printer</div>
+        <div className={` ${styles.img_box5} col-md-3 `}>Basket</div>
+        <div className={` ${styles.img_box6} col-md-3`}>Hanger</div>
+        <div className={` ${styles.img_box7} col-md-3`}>Scanner</div>
+     
+    </div>
+  </div>
+</section>
+
+
+
+      {/* <div className={styles.Flex_Section}>
+        <div className={styles.img_box}>Laundery Machine & Equipments</div>
+        <div className={styles.img_box1}>Dryclean Machine & Equipment</div>
+        <div className={styles.img_box2}>Laundry Bags</div>
+        <div className={styles.img_box3}>Chemical & Detergent</div>
+      </div>
+
+      <div className={styles.Flex_Section}>
+        <div className={styles.img_box4}>QR code printer</div>
+        <div className={styles.img_box5}>Basket</div>
+        <div className={styles.img_box6}>Hanger</div>
+        <div className={styles.img_box7}>sss</div>
+      </div> */}
+    </div>
+
+
+
+  );
+};
+
+export default AllCategory;
