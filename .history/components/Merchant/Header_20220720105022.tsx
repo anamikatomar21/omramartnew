7606f0bba82c4290eb0e62c5e71c034c8aeab4be
unknown <anamika.tomar@epicwebtechno.in>
2022-07-20 11:48:24 +0530

    frontend chnges

---
+++ .history/components/Merchant/Header_20220720105022.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+import { NextPage } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useCookies } from "react-cookie";
+import { handleLogout } from "redux/actions/auth";
+import { useAppDispatch } from "redux/hooks";
+import styles from "styles/Merchant/Header.module.scss";
+
+const Header: NextPage = () => {
+  const [, , removeCookie] = useCookies(["jwt"]);
+  // const [, dispatch] = useStateValue();
+  // const history = useHistory();
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+
+  const Logout = () => {
+    dispatch(handleLogout());
+    router.push("/index1");
+  };
+  return (
+    <div className={styles.navbarheader}>
+      <div className={styles.brandlogo}>
+        <div className={styles.logo}>
+          <Image
+            data-lazyloaded="1"
+            src="/logo.png"
+            height={50}
+            width={60}
+            alt="Logo Image"
+            className={styles.imageLogo}
+          />
+        </div>
+      </div>
+      <div className={styles.searchbox}>
+        <div className={styles.searchinnerbox}>
+          <input
+            type="text"
+            placeholder="Search.."
+            className={styles.searchinput}
+          />
+          <button type="submit" className={styles.searchinput1}>
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
+      <div className={styles.navbarright}>
+        <div className={styles.rightnav}>
+          <ul className={styles.navlinks}>
+            <li></li>
+            <li>
+              <i className="fas fa-user"></i>
+              <a href="#">Buyer</a>
+            </li>
+           
+            <li>
+              {/* <i className="fas fa-user"></i>  */}
+              <span className={css.profilr}>Profile</span>
+              <div className={styles.dropdown}>
+                <ul>
+                  <li>Company ID</li>
+                  <li>Edit Company Details</li>
+                  <li>Business Details</li>
+                </ul>
+              </div>
+            </li>
+
+            <li>
+              <div onClick={Logout}>Logout</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
---
+++ .history/components/Merchant/Layout_20220704182453.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import Header from './Header';
+import MerchantSidebar from './Sidebar';
+
+// }
+type DashboardLayoutProps = {
+  children: React.ReactNode,
+};
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+  //  <div className={styles.mmainlayout}>
+  //     <Header />
+  //     <div className={styles.mainlayout}>
+
+  //     {/* <MerchantSidebar /> */}
+  //       <main>{children}</main>
+  //     </div>
+      
+  //     {/* <Footer /> */}
+  //     </div>
+  <>
+    <Header />
+  <div style={{ display: "flex"  }}>
+  <div className="row">
+    <div className="col-6" style={{marginTop:"0px"}}>
+      <MerchantSidebar />
+    </div>
+   
+  </div>
+ 
+  <main style={{ width: "100%",marginTop:"-50px", marginLeft:"0px" }}>
+    <section>{children}</section>
+  </main>
+
+ 
+</div></>
+
+
+      
+  );
+}
+// const Layout({ children }):DashboardLayoutProps {
+//   return (
+//     <>
+
+// <div className='headersection'><Header /></div>
+// <div className='minsection'>
+// <div className='leftsidebar'><MerchantSidebar /></div>
+// <div className='rightsidebar'>({ children })</div>
+// </div>
+
+
+      
+      
+//       <Footer />
+//     </>
+//   )
+// }
+
