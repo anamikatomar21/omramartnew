04699cc370bb91bec95da4ae5cacea3fa16572c0
unknown <anamika.tomar@epicwebtechno.in>
2022-07-20 18:34:38 +0530

    frontend chnges

---
+++ .history/components/Category/index_20220720171236.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { useGetHomeCategory, usePublicProduct } from "../../networkAPI/queries";
+import styles from "../../styles/Merchant/view.module.scss";
+
+function Category() {
+  const { data, status } = usePublicProduct();
+  const data2 = useGetHomeCategory();
+  const category_data = data2.data;
+  console.log(data2.data);
+  console.log(data);
+  return (
+    di
+    <div>
+      <h1 className={styles.heading_section}>All Category</h1>
+      <div>
+        <div key={"index"} style={{ display: "flex" }}>
+          <div className={styles.Flex_Section}>
+            {category_data?.data.map((item3: any, index: any) => {
+              console.log(item3);
+
+              return (
+                <div key={index}>
+                  <div
+                    className={styles.img_box}
+                    style={{
+                      backgroundImage: `url(${item3.category_image[0]})`,
+                    }}
+                  >
+                    {item3.category_name}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
---
+++ .history/components/SliderBox/index_20220720172857.tsx
@@ -0,0 +1,56 @@
+import {
+
+    useEffect,
+
+    useState,
+
+} from 'react';
+import styles from '../../styles/Merchant/sliderbox.module.scss';
+import Image from 'next/image';
+
+import type { NextPage } from 'next';
+
+import { useRouter } from 'next/router';
+
+
+import Link from 'next/link';
+
+
+
+
+
+
+
+import React, { Component } from 'react';
+
+
+
+const SliderBox: NextPage = () => {
+
+
+    return (
+<div className={styles.Flex_Section}>
+<div>
+    <Image
+    src="/omratrade/sliderbanner.png"
+    width={700} height={400} alt="facemask"
+    className="facemask"
+  />
+  </div>
+  <div>
+  <Image
+  src="/omratrade/sliderbanner2.png"
+  width={800} height={400} alt="facemask"
+  className="facemask"
+/>
+</div>
+</div>
+    )
+
+};
+
+
+
+export default SliderBox;
+
+
